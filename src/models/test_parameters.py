baec929749b91f739108c4e178ccf2533ca69c46
Mikołaj Sacha <mikolaj.sacha@ericpol.com>
2016-11-30 11:33:02 +0100

    #9 Automatic computation of cross-validation results on all possible parameters from some range (naive approach)

--- src/models/test_parameters.py
+++ src/models/test_parameters.py
@@ -5,6 +5,7 @@ from src.features.sentence_embeddings import sentence_embeddings
 from src.models.algorithms import validation
 from src.models.algorithms.svm_algorithm import SvmAlgorithm
 from src.data import make_dataset
+import itertools
 import os
 
 
@@ -17,13 +18,14 @@ def test_parameters(data_folder, folds_count, classifier, sentence_embedding, wo
                                             feature_builder, classifier, folds_count, **kwargs)
 
 
-def test_all_params_combinations(param_name, data_folder, folds_count, **kwargs):
+def test_all_params_combinations(data_folder, folds_count, **kwargs):
     sentence_embeddings = kwargs['sentence_embeddings']
     word_embeddings = kwargs['word_embeddings']
     classifiers = kwargs['classifiers']
-    min_param_power = kwargs['min_param_power']
-    max_param_power = kwargs['max_param_power']
     tested_params = kwargs['params']
+
+    params_values = ([(param, 10**i) for i in xrange(*bounds)] for param, bounds in tested_params.iteritems())
+    all_combinations = map(lambda tuple_list: dict(tuple_list), itertools.product(*params_values))
 
     best_result = 0.0
     best_results_descriptions = None
@@ -39,18 +41,17 @@ def test_all_params_combinations(param_name, data_folder, folds_count, **kwargs)
                 best_for_classifier_params = None
                 classifier_desc = ','.join(map(lambda n: type(n).__name__, [classifier, word_emb, sen_emb]))
                 classifiers_descriptions.append(classifier_desc)
-                for params in []: # TODO: znajdz sposob na wszystkie kombinacje parametrow jako lista/generator
+                for params in all_combinations:
                     print("." * 20)
                     print("Testing " + classifier_desc)
-                    result = test_parameters(param_name, data_folder, folds_count, classifier,
-                                             sen_emb, word_emb, min_param_power, max_param_power, **params)
+                    result = test_parameters(data_folder, folds_count, classifier, sen_emb, word_emb, **params)
                     if result > best_for_classifier_result:
                         best_for_classifier_result = result
                         best_for_classifier_params = [params]
                     elif result == best_for_classifier_result:
                         best_for_classifier_params.append(params)
 
-                    result_desc = classifier_desc + ", params = " + params
+                    result_desc = classifier_desc + ", params = " + str(params)
                     if result > best_result:
                         best_result = result
                         best_results_descriptions = [result_desc]
@@ -64,14 +65,14 @@ def test_all_params_combinations(param_name, data_folder, folds_count, **kwargs)
     print("Overall results:")
     print("." * 20)
     for i in xrange(len(classifiers_results)):
-        print "Best result for classifier: {:s} is {:4.2f}% is for parameterss: " \
+        print "Best result for classifier: {:s} is {:4.2f}% is for parameters: " \
             .format(classifiers_descriptions[i], classifiers_results[i])
-        for params in xrange(len(classifiers_params[i])):
+        for params in classifiers_params[i]:
             print str(params)
 
     print("." * 20)
     print("." * 20)
-    print ("Best cross-validation result is {:4.2f}% for the following cases:".format(best_result))
+    print ("Overall best cross-validation result is {:4.2f}% for the following cases:".format(best_result))
     print("." * 20)
     for desc in best_results_descriptions:
         print desc
@@ -80,7 +81,6 @@ def test_all_params_combinations(param_name, data_folder, folds_count, **kwargs)
 if __name__ == "__main__":
     data_folder = "dataset3_reduced"
     folds_count = 5
-    gamma_powers_range = -3, -1
 
     input_file_path = make_dataset.get_external_data_path(data_folder)
     output_file_path = make_dataset.get_processed_data_path(data_folder)
@@ -100,7 +100,6 @@ if __name__ == "__main__":
 
     classifiers = [SvmAlgorithm()]
 
-    test_all_params_combinations("gamma", data_folder, folds_count, word_embeddings=word_embeddings,
+    test_all_params_combinations(data_folder, folds_count, word_embeddings=word_embeddings,
                                  sentence_embeddings=sentence_embeddings, classifiers=classifiers,
-                                 min_param_power=gamma_powers_range[0], max_param_power=gamma_powers_range[1],
-                                 other_params={"C": 100})
+                                 params={"C": (1, 3), "gamma": (-3, -1)})
